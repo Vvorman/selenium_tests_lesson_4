169eaddd6e8acccc0d4a76f2627df31580e68746
Daria <vvorman@gmail.com>
2025-04-11 23:28:33 +0300

    Add tests for authorized user to add product to basket

---
+++ pages/basket_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+class BasketPage(BasePage):
+    def should_be_empty_basket_message(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), \
+            "Empty basket message is not presented"
+
+    def should_not_be_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
+            "Basket contains items, but should be empty"
